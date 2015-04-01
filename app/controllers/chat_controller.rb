6a7a6eadf1d4bd5dcae7ef0fbe5b5db029640dce
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2015-04-01 19:18:14 -0400

    get chats

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,3 +1,10 @@
 class ChatController < ApplicationController
 skip_before_filter  :verify_authenticity_token
+
+  def getChats
+
+    @chats = ChatsWith.where("owner_1 = ? or owner_2 = ?", params[:user_id], params[:user_id]).order("started_at desc").all
+
+  end
+
 end
